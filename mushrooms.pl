a10d2d0f8d4e1e346dc61a061b42b8c1ee9cad80
Julia Plewa <juliadplewa@gmail.com>
2020-11-18 01:25:39 +0100

    Add right subree predicates

--- mushrooms.pl
+++ mushrooms.pl
@@ -5,14 +5,9 @@
 mushroom_is(edible) :- positive(bruises, presence, visible),
                        stalk_root(shape, club).
 
-% TODO: add category
-mushroom_is(unknown) :- negative(bruises, presence, visible),
+mushroom_is(edible) :- negative(bruises, presence, visible),
                         gills(spacing, crowded),
                         stalk_root(shape, equal).
-% TODO: add category
-mushroom_is(unknown) :- negative(bruises, presence, visible),
-                        \+gills(spacing, close),
-                        \+gills(spacing, crowded).
 
 mushroom_is(edible) :- negative(bruises, presence, visible),
                        gills(spacing, crowded),
@@ -49,8 +44,75 @@ mushroom_is(edible) :- negative(bruises, presence, visible),
 
 mushroom_is(edible) :- negative(bruises, presence, visible),
                        gills(spacing, close),
-                       stalk_root(shape, club),
-                       stalk_above_ring(color, white).
+                       veil(color, brown).
+
+mushroom_is(edible) :- negative(bruises, presence, visible),
+                       gills(spacing, close),
+                       veil(color, orange).
+
+% TODO: double-check
+mushroom_is(edible) :- negative(bruises, presence, visible),
+                       gills(spacing, close),
+                       veil(color, yellow).
+
+mushroom_is(poisonous) :- negative(bruises, presence, visible),
+                          gills(spacing, close),
+                          veil(color, white),
+                          stalk_root(shape, club).
+
+mushroom_is(poisonous) :- negative(bruises, presence, visible),
+                          gills(spacing, close),
+                          veil(color, white),
+                          stalk_root(shape, bulbous),
+                          rings(number, none).
+
+mushroom_is(edible) :- negative(bruises, presence, visible),
+                       gills(spacing, close),
+                       veil(color, white),
+                       stalk_root(shape, bulbous),
+                       rings(number, two).
+
+mushroom_is(edible) :- negative(bruises, presence, visible),
+                       gills(spacing, close),
+                       veil(color, white),
+                       stalk_root(shape, bulbous),
+                       rings(number, one),
+                       cap(color, white),
+                       cap(surface, scaly).
+
+mushroom_is(poisonous) :- negative(bruises, presence, visible),
+                          gills(spacing, close),
+                          veil(color, white),
+                          stalk_root(shape, bulbous),
+                          rings(number, one),
+                          cap(color, white),
+                          cap(surface, fibrous).
+
+mushroom_is(poisonous) :- negative(bruises, presence, visible),
+                          gills(spacing, close),
+                          veil(color, white),
+                          stalk_root(shape, bulbous),
+                          rings(number, one),
+                          cap(color, white),
+                          cap(surface, smooth).
+
+mushroom_is(poisonous) :- negative(bruises, presence, visible),
+                          gills(spacing, close),
+                          veil(color, white),
+                          stalk_root(shape, equal),
+                          cap(surface, scaly).
+
+mushroom_is(poisonous) :- negative(bruises, presence, visible),
+                          gills(spacing, close),
+                          veil(color, white),
+                          stalk_root(shape, equal),
+                          cap(surface, smooth).
+
+mushroom_is(edible) :- negative(bruises, presence, visible),
+                       gills(spacing, close),
+                       veil(color, white),
+                       stalk_root(shape, equal),
+                       cap(surface, fibrous).
 
 stalk_root(X, Y) :- positive(stalk_root, X, Y).
 
@@ -61,6 +123,10 @@ stalk_above_ring(X, Y) :- positive(stalk_above_ring, X, Y).
 stalk_below_ring(X, Y) :- positive(stalk_below_ring, X, Y).
 
 rings(X, Y) :- positive(rings, X, Y).
+
+veil(X, Y) :- positive(veil, X, Y).
+
+cap(X, Y) :- positive(cap, X, Y).
 
 % TODO: double check if positive/3 & negative/3 both work as intended
 positive(X, Y, Z) :- xpositive(X, Y, Z), !.
@@ -74,21 +140,22 @@ negative(X, Y, Z) :- \+xpositive(X, Y, Z), ask(X, Y, Z, no).
 
 other_match_exists(X, Y, Z) :- xpositive(X, Y, W), Z \= W, !.
 
-% TODO: find a way to replace multiple underscores
+replace_underscores(X, X) :- re_replace("_", " ", X, X), !.
+replace_underscores(X, Y) :- re_replace("_", " ", X, X1), replace_underscores(X1, Y).
+
 ask(X, Y, Z, yes) :- !, 
-                     re_replace("_", " ", X, X1),
-                     re_replace("_", " ", Y, Y1),
-                     re_replace("_", " ", Z, Z1),
+                     replace_underscores(X, X1),
+                     replace_underscores(Y, Y1),
+                     replace_underscores(Z, Z1),
                      format('Is the ~w of ~w ~w? (y/n)~n', [Y1, X1, Z1]),
                      read(Reply),
                      memorize(X, Y, Z, Reply),
                      (Reply = 'y').
 
-% TODO: find a way to replace multiple underscores
 ask(X, Y, Z, no) :- !,
-                    re_replace("_", " ", X, X1),
-                    re_replace("_", " ", Y, Y1),
-                    re_replace("_", " ", Z, Z1),
+                    replace_underscores(X, X1),
+                    replace_underscores(Y, Y1),
+                    replace_underscores(Z, Z1),
                     format('Is the ~w of ~w ~w? (y/n)~n', [Y1, X1, Z1]),
                     read(Reply),
                     memorize(X, Y, Z, Reply),
